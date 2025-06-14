d112bbd0947de2fa5a1461b41a5e416b1d6c5b8b
yashbhangale <yashbhangale9@gmail.com>
2025-06-14 13:44:46 +0530

    Quartz sync: Jun 14, 2025, 1:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "yashbhangale.github.io/vault/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
